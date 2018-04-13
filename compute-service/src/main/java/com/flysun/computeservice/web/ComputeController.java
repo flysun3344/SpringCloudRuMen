b73a69b10b29287a12ab2b413b0a32ef2e830a6b
sunfei <sunfei@ppcredit.com>
2018-04-13 11:39:53 +0800

    修改

--- compute-service/src/main/java/com/flysun/computeservice/web/ComputeController.java
+++ compute-service/src/main/java/com/flysun/computeservice/web/ComputeController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
+ * kl
  * Created by sunfei on 2018/3/29.
  */
 
